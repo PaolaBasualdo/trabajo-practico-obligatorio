a4b9924c3ec045b79b0e70fd61e61416518e746d
PaolaBasualdo <117169838+PaolaBasualdo@users.noreply.github.com>
2023-10-08 12:18:39 -0300

    Completar secciones Actualidad y Vigilancia

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ function toggleMenu() {
     x.className = "navbar";
   }
 }
+
+function initMap() {
+  var ubicacion = {lat: -34.397, lng: 150.644}; // Cambia las coordenadas según la ubicación que desees mostrar
+  var mapa = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8, // Nivel de zoom
+    center: ubicacion // Ubicación del centro del mapa
+  });
+  var marcador = new google.maps.Marker({
+    position: ubicacion,
+    map: mapa,
+    title: 'Mi Ubicación' // Título del marcador (opcional)
+  });
+}
